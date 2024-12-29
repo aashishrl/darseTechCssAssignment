9d9cc64a45d918ea936e605b91a7c7baf9c67215
Aashish Ramtel <aaseshrl@gmail.com>
2024-12-29 21:58:44 +0545

    css assignment completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div class="boxes-container">
+        <div class="box box1 samecss">Widget 1</div>
+        <div class="box box2 samecss">Widget 2</div>
+        <div class="box box3 samecss">Widget 3</div>
+        <div class="box box4 samecss">Widget 4</div>
+        <div class="box box5 samecss">Widget 5</div>
+        <div class="box box6 samecss">Widget 6</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
