e741695eea84d10151f77d8412283bc96cd36496
Aashish Ramtel <aaseshrl@gmail.com>
2024-12-29 22:00:44 +0545

    fix: added calssname

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function App() {
   return (
     <>
-      <div class="boxes-container">
-        <div class="box box1 samecss">Widget 1</div>
-        <div class="box box2 samecss">Widget 2</div>
-        <div class="box box3 samecss">Widget 3</div>
-        <div class="box box4 samecss">Widget 4</div>
-        <div class="box box5 samecss">Widget 5</div>
-        <div class="box box6 samecss">Widget 6</div>
+      <div className="boxes-container">
+        <div className="box box1 samecss">Widget 1</div>
+        <div className="box box2 samecss">Widget 2</div>
+        <div className="box box3 samecss">Widget 3</div>
+        <div className="box box4 samecss">Widget 4</div>
+        <div className="box box5 samecss">Widget 5</div>
+        <div className="box box6 samecss">Widget 6</div>
       </div>
     </>
   );
